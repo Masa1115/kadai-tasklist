1cf2be1d48256ec1dc2d19df4d019387f32190eb
masaki.shiga <ma15sa99ma24@gmail.com>
2020-05-15 11:22:21 +0000

    status

--- db/migrate/20200515015659_add_to_status.rb
+++
@@ -1,4 +0,0 @@
-class AddToStatus < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_015659) do
+ActiveRecord::Schema.define(version: 2020_05_15_110655) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.string "status"
   end
 
 end
